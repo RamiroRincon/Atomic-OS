ce8ce64d5f3d88609efa69d12d38550dca0674ed
Ramiro R. <77977931+RamiroRincon@users.noreply.github.com>
2025-12-06 00:37:10 -0300

    Create 01-remove-bloat.sh

---
+++ scripts/01-remove-bloat.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eox pipefail
+
+# List of RPMs to remove
+# We use 'override remove' because they are likely part of the base layer
+rpm-ostree override remove \
+    firefox \
+    firefox-langpacks \
+    gnome-tour \
+    gnome-clocks \
+    gnome-maps \
+    gnome-weather \
+    gnome-contacts \
+    gnome-calculator \
+    gnome-characters \
+    gnome-logs \
+    gnome-font-viewer \
+    gnome-disk-utility \
+    baobab \
+    evince \
+    eog \
+    loupe \
+    snapshot \
+    simple-scan \
+    totem \
+    yelp \
+    htop \
+    nvtop \
+    fedora-media-writer
