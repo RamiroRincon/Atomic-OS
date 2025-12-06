fe1542857d1031c41f7582f0c56fe9905007619e
Ramiro R. <77977931+RamiroRincon@users.noreply.github.com>
2025-12-06 00:36:38 -0300

    Create 00-install-gnome.sh

---
+++ scripts/00-install-gnome.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -eox pipefail
+
+# Install Core GNOME
+# specific packages to ensure minimal bootable desktop
+rpm-ostree install \
+    gdm \
+    gnome-shell \
+    nautilus \
+    gnome-console \
+    gnome-software \
+    mutter \
+    gnome-session \
+    gnome-settings-daemon \
+    gnome-control-center \
+    xdg-desktop-portal-gnome \
+    xdg-user-dirs-gtk \
+    adwaita-icon-theme \
+    gsettings-desktop-schemas
+
+# GDM Configuration (Wayland)
+mkdir -p /etc/gdm
+cat >/etc/gdm/custom.conf <<EOF
+[daemon]
+WaylandEnable=true
+EOF
+
+# Enable Services
+systemctl enable gdm.service
+systemctl set-default graphical.target
