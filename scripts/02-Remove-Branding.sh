7b6900c6245efe103a2a3b01a0c9b51f87327d4f
Ramiro R. <77977931+RamiroRincon@users.noreply.github.com>
2026-01-08 23:43:43 -0300

    Update #051

--- scripts/02-Remove-Branding.sh
+++ scripts/02-Remove-Branding.sh
@@ -24,3 +24,6 @@ done
 if grep -q 'PRETTY_NAME="Fedora' /usr/lib/os-release; then
     sed -i 's/PRETTY_NAME="Fedora[^"]*"/PRETTY_NAME="Atomic-OS"/g' /usr/lib/os-release
 fi
+
+echo "Atomic-OS \n \l" > /etc/issue
+echo "Atomic-OS \n \l" > /etc/issue.net
